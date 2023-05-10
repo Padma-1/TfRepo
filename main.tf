47eb8d2c79abae76a1d68b4d3f905e4cdd8ca8ef
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2023-05-10 15:43:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAYOU3NUV2QULPOWZB"
-  secret_key = "1RzDFbL1CXbZCK3IbVzUk5UjCq00u2U7YYu9McAR"
+  access_key = "<UrKey>"
+  secret_key = "<GiveUrKey>"
   region     = "us-east-1"
 }
 resource "aws_instance" "example" {
